bd96da2c0fba34d172568c1d08818039e1adbb87
Mark Wlodawski <imakethesites@gmail.com>
2021-01-31 23:13:46 -0500

    AssetPrefix added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
+const isProd = process.env.NODE_ENV === "production";
 const withImages = require("next-images");
-module.exports = withImages();
+// module.exports = withImages();
 module.exports = {
-  images: {
-    domains: ["http://52.70.112.177/"],
-  },
+  assetPrefix: isProd ? "http://52.70.112.177/" : "",
 };
