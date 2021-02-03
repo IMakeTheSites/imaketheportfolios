ef99fd77f92f5217a906875fc23b81f9e35b6e4c
Mark Wlodawski <imakethesites@gmail.com>
2021-02-02 23:06:35 -0500

    Delete imts-next-portfolio-master directory

--- imts-next-portfolio-master/next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === "production";
-const withImages = require("next-images");
-// module.exports = withImages();
-module.exports = {
-  assetPrefix: isProd ? "http://52.70.112.177/" : "",
-};
